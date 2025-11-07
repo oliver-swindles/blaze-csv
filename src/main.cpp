f39b2e80162249ce18538a1821d6d92e3c042f1d
Oliver Swindles <86194326+oliver-swindles@users.noreply.github.com>
2025-11-07 10:49:34 +0000

    Refactor: Separated processing logic from main function

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,48 @@ vector<string> split(const string& str, char delimiter) {
     return tokens;
 }
 
+struct Results {
+    long totalCount = 0;
+    long validDataCount = 0;
+    double totalSum = 0;
+};
+
+Results processFile(ifstream& file, int columnIndex, const string& operation) {
+    Results results;
+    bool needsParsing = (operation == "sum" || operation == "average");
+
+    string fileDataLine;
+
+    while (getline (file, fileDataLine)) {
+        results.totalCount++;
+
+        if (!needsParsing) {
+            continue;
+        }
+
+        stringstream lineStream(fileDataLine);
+        string dataChunk;
+        int currentColumn = 0;
+
+
+        while (getline(lineStream, dataChunk, ',')) {
+            if (currentColumn == columnIndex) {
+                try {
+                    results.totalSum += stod(dataChunk);
+                    results.validDataCount++;
+                }
+                catch (const invalid_argument& e) {
+                }
+                catch (out_of_range& e) {
+                }
+                break;
+            }
+            currentColumn++;
+        }
+    }
+    return results;
+};
+
 int main(int argc, char** argv) {
     // Read args
     // In format [program] [filename.csv] [column name] [operation]
@@ -60,37 +102,18 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    Results fileResults = processFile(file, columnIndex, operation);
+
     // Print rest of file
     string fileDataLine;
     double totalSum = 0;
     long validDataCount = 0;
     long totalCount = 0;
-    while (getline (file, fileDataLine)) {
-        totalCount++;
-        stringstream lineStream(fileDataLine);
-        string dataChunk;
-        int currentColumn = 0;
 
-        while (getline(lineStream, dataChunk, ',')) {
-            if (currentColumn == columnIndex) {
-                try {
-                    totalSum += stod(dataChunk);
-                    validDataCount++;
-                }
-                catch (const invalid_argument& e) {
-                }
-                catch (out_of_range& e) {
-                }
-                break;
-            }
-            currentColumn++;
-        }
-    }
-
-    cout << "Total Count: " << totalCount << endl;
-    cout << "Valid Count: " << validDataCount << endl;
-    cout << "Sum: " << totalSum << endl;
-    cout << "Average: " << (totalSum / validDataCount) << endl;
+    cout << "Total Count: " << fileResults.totalCount << endl;
+    cout << "Valid Count: " << fileResults.validDataCount << endl;
+    cout << "Sum: " << fileResults.totalSum << endl;
+    cout << "Average: " << (fileResults.totalSum / fileResults.validDataCount) << endl;
     file.close();
     return 0;
 }
